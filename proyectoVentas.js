94676ef328160d30f39694a33756f236b748f492
JavierP <zlaattan27@gmail.com>
2023-06-14 12:13:42 -0500

    Se crea la clase Orden

--- proyectoVentas.js
+++ proyectoVentas.js
@@ -30,3 +30,48 @@ class Producto{
     }
     
 }
+
+class Orden{
+    static contadorOrdenes = 0;
+    static get MAX_PRODUCTOS(){
+        return 5;
+    }
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.length < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+
+
+        }else{
+            console.log('no se pueden agregar mas productos')
+        }
+
+    }
+
+    calcularTotal(){
+        let totalVenta=0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio;
+        }
+        return totalVenta;
+    }
+
+    mostrarOrden(){
+        let productosOrden = '';
+        for(let producto of this._productos){
+            productosOrden += '\n{'+ producto.toString()+'}';
+        }
+        console.log(`Orden: ${this._idOrden} Total: $${this.calcularTotal()}, Productos: ${this.productosOrden} `)
+    }
+    
+}
